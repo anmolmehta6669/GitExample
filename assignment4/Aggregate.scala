1d1c39f6fd8396ebb47229a73b91e7d01dc77f47
Jatin9896 <jatin.demla@knoldus.in>
2017-01-20 11:43:36 +0530

    adding programs

--- assignment4/Aggregate.scala
+++ assignment4/Aggregate.scala
@@ -6,12 +6,17 @@ object Aggregate{
 def main(args:Array[String]):Unit={
 	val list1= List(1,2,3,4,5)
 	val list2=List('a','b','c','d','e')
-	var list3=List((list1(0),list2(0)))
+	/*var list3=List((list1(0),list2(0)))
 	for(i<- 1 to list1.length-1) list3=list3:::List((list1(i),list2(i)))
 	list3.map(println _)
+	}*/
+
+	val list3=recursiveAggregate(list1,list2)
+	list3 map (println _)
+
 	}
-
-
-//	def aggregate()
+	def recursiveAggregate(list1: List[Int], list2: List[Char]):List[(Int,Char)]={
+		def recAgg()
+	}
 
 }
